47de8c6d0ec1d38cb0a46b32ff63e25a998dd2dd
100rabh patil <99138605+100rab100@users.noreply.github.com>
2022-11-25 20:43:21 +0530

    Update Increase size of array.java

--- Increase size of array.java
+++ Increase size of array.java
@@ -1,20 +1,36 @@
 
-public class SCArray3 
-{
-    public static void main(String[] args) 
-    {
-        int A[]={8,6,10,9,2};
-        
-        System.out.println("Length of A="+A.length);
-        
-        int B[]=new int[10];
-        
-        for(int i=0;i<A.length;i++)
-        {
-            B[i]=A[i];
-        }
-        A=B;
-        
-        System.out.println("Length of A="+A.length);
-        
-    }
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String args[]){
+
+                int A[]={8,6,10,9,2};
+
+                System.out.println("Length of A="+A.length);
+
+                int B[]=new int[10];
+
+                for(int i=0;i<A.length;i++)
+                {
+                    B[i]=A[i];
+                }
+                A=B;// NOW B ref to null and A ref to B old wala array  that is of 10 size here .
+
+                System.out.println("Length of B="+A.length);
+
+            }
+
+
+
+
+
+}
+
+
+
+
+
