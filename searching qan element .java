04dd227ad0d6d1f7344c316c53c25745e47cd67e
100rabh patil <99138605+100rab100@users.noreply.github.com>
2022-11-25 19:17:14 +0530

    Update searching qan element .java

--- searching qan element .java
+++ searching qan element .java
@@ -1,25 +1,39 @@
 
-     
-public class first
-{
-
-    public static void main(String[] args)
-    {
-        int A[]={1,2,3,4,56,7,8};
-        System.out.println("Eneter a Key " );
-        Scanner t=new Scanner(System.in);
-       int key=t.nextInt();
 
 
-        for(int i=0;i<A.length;i++)
-        {
-            if(key==A[i])
-            {
-                System.out.println("Element Found at :"+i);
-                System.exit(0);
-            }
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]){
+
+        System.out.println("Enter the required size of the array :: ");
+        Scanner t = new Scanner(System.in);
+        int size = t.nextInt();
+        int a[]=new int[size];
+        int key=10;
+        System.out.println("Enter the elements of the array one by one ");
+
+        for(int i=0;i<size;i++){
+            a[i]=t.nextInt();
         }
-        System.out.println("Not found");
+        for(int i=0;i<size;i++){
+            if( a[i]==key){
+                System.out.println("key is found");
+                System.exit(0);
+
+
+            }
+
+        }
+        System.out.println("NOt found");
+
+
+        System.out.println("Elements of the array are: "+Arrays.toString(a));
+
 
     }
+
 }
+
+
