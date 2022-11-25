84731d76b3ad2d2bb6c211a4294064f7a08628d0
100rabh patil <99138605+100rab100@users.noreply.github.com>
2022-11-25 19:07:07 +0530

    Update Finding Sum of Elements.java

--- Finding Sum of Elements.java
+++ Finding Sum of Elements.java
@@ -15,3 +15,32 @@ public static void main(String[] args)
         
     }  
 }
+//
+
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]){
+
+        System.out.println("Enter the required size of the array :: ");
+        Scanner t = new Scanner(System.in);
+        int size = t.nextInt();
+        int a[]=new int[size];
+        int sum=0;
+        System.out.println("Enter the elements of the array one by one ");
+
+        for(int i=0;i<size;i++){
+            a[i]=t.nextInt();
+            sum=sum+a[i];
+
+        }
+        System.out.println("Elements of the array are: "+Arrays.toString(a));
+
+        System.out.println("Sum is "+sum);
+    }
+
+}
+
+
